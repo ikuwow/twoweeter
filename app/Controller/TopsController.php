abafe558532afcbf6ce64b5e5acec7e09be61e2d
Ikuo Degawa <degawa@ikuwow.com>
2014-03-02 10:12:38 +0900

    redirect when login from top

--- app/Controller/TopsController.php
+++ app/Controller/TopsController.php
@@ -5,6 +5,9 @@ class TopsController extends AppController {
 
 
     public function index() {
+        if ($this->Session->read('me')) {
+            $this->redirect(array('controller'=>'mypages','action'=>'timeline'));
+        }
         $this->layout = 'toplayout';
     }
 
