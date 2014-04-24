719a3c1914153715470eb28fdd880b3ef0ecfefe
ikuwow <degawa@ikuwow.com>
2014-04-24 14:04:43 +0900

    scrap build script, test & default

--- sql/scrap_build.sql
+++
@@ -1,9 +0,0 @@
-# scrap & bulid sql
-# 
-
-drop database if exists twoweeter;
-create database if not exists twoweeter;
-grant all on twoweeter.* to developer@localhost;
-
-use twoweeter;
-source schema.sql;
