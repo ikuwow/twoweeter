9afc8a139f6ba4a4cb6d1edcb234a0e178f6ae41
ikuwow <degawa@ikuwow.com>
2014-04-24 22:53:40 +0900

    use shema.php instead of sql files

--- sql/test_reset.sql
+++
@@ -1,9 +0,0 @@
-# scrap & bulid sql
-# 
-
-drop database if exists test_twoweeter;
-create database if not exists test_twoweeter;
-grant all on test_twoweeter.* to tester@localhost;
-
-use test_twoweeter;
-source schema.sql;
